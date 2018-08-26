51e3254e33ffca703f6b6aa5da633387572da8e5
maurelian <maurelian@protonmail.ch>
2018-08-26 13:54:39 -0400

    refine storage var model

--- src/utils/contractProfiler.js
+++ src/utils/contractProfiler.js
@@ -12,7 +12,6 @@ const parser = require('solidity-parser-antlr')
 function contractProfilesFromFile(file) {
   let contractProfile = new Object() // Info about a contract
   let contractProfiles = new Array() // List of contractProfiles defined in a file
-  let functionProfile = new Object() // Info about a function
   let functionProfiles = new Array() // List of functionsProfiles defined in a contract
   let stateVarProfiles = new Array() // List of stateVarProfiles defined in a contract
 
@@ -29,38 +28,47 @@ function contractProfilesFromFile(file) {
       })
 
       let kind = node.kind
-
       Object.assign(contractProfile, {name, bases, kind})
     },
 
-    'ContractDefinition:exit': function(node) {
-      // Done visiting this contract
-      Object.assign(contractProfile, { stateVarProfiles, functionProfiles }) // record function details
-
-      contractProfiles.push(contractProfile) // add to list of contractProfiles
-      contractProfile = new Object() // reset the contractProfile object
+    'ContractDefinition:exit': function(node){
+      Object.assign(contractProfile, {stateVarProfiles, functionProfiles})
+      contractProfiles.push(contractProfile)
     },
 
     StateVariableDeclaration(node) {
-      
+      let statVarProfile = new Object();
       // The parser returns a 'variables' array here. But is it possible there could ever be 
       // more than one entry? I don't see how.
       let {name, visibility, typeName} = node.variables[0]
-      /*"typeName": {
-                  "type": "ElementaryTypeName",
-                  "name": "address"
-                }
-      */
 
-      // if (typeName.type === 'Mapping') {
+      // typeName grammar:
+      // https://github.com/solidityj/solidity-antlr4/blob/master/Solidity.g4#L126
+      // typeName
+      //  : elementaryTypeName // done
+      //  | userDefinedTypeName // make visitor
+      //  | mapping // tricky, can be nested
+      //  | typeName '[' expression? ']' // how to identify this?
+      //  | functionTypeName ; // make visitor
 
-      // } else if (typeName.type === 'userDefinedTypeName') {
-      //   // don't think there's anything to do here.
-      // } else if (typeName.type === typeName '[' expression? ']') { // how to check for array pattern in name?
+      // TODO: create a test contract with a wide range of type declarations. Use that for testing.
+      parser.visit(typeName, {
+        Mapping(subNode){
 
-      // } else if (typeName.type.indexof('function')) {
+          let keyType = subNode.keyType.name
+          let valueType
+          if (subNode.valueType !== 'Mapping') {
+            valueType = subNode.valueType.name
+          }
 
-      // } else if (typeName.)
+        },
+        userDefinedTypeName(subNode){
+          console.log('userDefinedTypeName')
+        }, 
+        functionTypeName(subNode){
+
+        }
+      })
 
       stateVarProfiles.push({name, visibility, type: typeName.type})
     },
@@ -84,15 +92,8 @@ function contractProfilesFromFile(file) {
       // only one mutability keyword is allowed: pure, view, constant, payable
       let mutability = node.stateMutability
 
-      Object.assign(functionProfile, {name, visibility, mutability})
+      functionProfiles.push({name, visibility, mutability})
 
-    }, 
-    
-    'FunctionDefinition:exit': function(node) {
-      // Note: I think this can all just be moved up to FunctionDefinition, and then the 
-      // profile Object wouldn't even be necessary.
-      functionProfiles.push(functionProfile)
-      functionProfile = new Object()
     }
   })
 
