754c7295d4d02b514ccd67a98c3db074bc4f02d7
Anurag Dashputre <anurag4u80@gmail.com>
2018-08-29 16:42:15 +0530

    commit

--- src/ftrace.js
+++ src/ftrace.js
@@ -1,12 +1,12 @@
 "use strict";
 
+const profiler = require('./utils/contractProfiler')
 const parserHelpers = require('./utils/parserHelpers')
 const fs = require('fs')
 const parser = require('solidity-parser-antlr')
 const colors = require('colors')
 const { linearize } = require('c3-linearization')
 const treeify = require('treeify')
-
 
 export function ftrace(functionId, accepted_visibility, files) {
   if (files.length === 0) {
@@ -35,45 +35,39 @@ export function ftrace(functionId, accepted_visibility, files) {
   let functionDecorators = {}
 
   let fileASTs = new Array()
+  let profiles
 
   for (let file of files) {
-
-    let content
-    try {
-      content = fs.readFileSync(file).toString('utf-8')
-    } catch (e) {
-      if (e.code === 'EISDIR') {
-        console.error(`Skipping directory ${file}`)
-        continue
-      } else throw e;
-    }
-
-    const ast = parser.parse(content)
-
-    fileASTs.push(ast)
-
     let contractName = null
-    let cluster = null
+    profiles = profiler.contractProfilesFromFile(file)
 
-    parser.visit(ast, {
-      ContractDefinition(node) {
-        contractName = node.name
+   for (let profile of profiles) {
+       const ast = profile.ast
+       fileASTs.push(ast)
 
-        userDefinedStateVars[contractName] = {}
+      // For ContractDefinition
+      let bases = profile.bases
+      contractName = profile.name
+      userDefinedStateVars[contractName] = {}
+      dependencies[contractName] = bases
+      // For ContractDefinition
 
-        dependencies[contractName] = node.baseContracts.map(spec =>
-          spec.baseName.namePath
-        )
-      },
+      // For ImportDirective
+      for (let importProfile of profile.importProfiles) {
+        const astImport = importProfile.astImport
+        fileASTs.push(astImport)
+      }
+      // For ImportDirective
 
-      StateVariableDeclaration(node) {
-        for (let variable of node.variables) {
-          if (parserHelpers.isUserDefinedDeclaration(variable)) {
-            userDefinedStateVars[contractName][variable.name] = variable.typeName.namePath
-          }
+      // For StateVariableDeclaration
+      for (let stateVariableProfile of profile.stateVariableProfiles) {
+        for (let variable of stateVariableProfile.variables) {
+           if (parserHelpers.isUserDefinedDeclaration(variable))
+             userDefinedStateVars[contractName][variable.name] = variable.typeName.namePath
         }
       }
-    })
+      // For StateVariableDeclaration
+   }
   }
 
   dependencies = linearize(dependencies, {reverse: true})
@@ -81,6 +75,225 @@ export function ftrace(functionId, accepted_visibility, files) {
   for (let ast of fileASTs) {
 
     let contractName = null
+    let libraryName = null
+    let functionName = null
+    let cluster = null
+
+    let userDefinedLocalVars = {}
+    let tempUserDefinedStateVars = {}
+
+    for (let profile of profiles) {
+
+      // For ContractDefinition
+      contractName = profile.name
+   //   console.log("contractName="+contractName)
+
+       if(profile.kind == "library")
+         dependencies[contractName] = profile.name
+
+       functionCallsTree[contractName] = {}
+       modifiers[contractName] = {}
+
+       for (let dep of dependencies[contractName]) {
+         Object.assign(tempUserDefinedStateVars, userDefinedStateVars[dep])
+       }
+
+       Object.assign(tempUserDefinedStateVars, userDefinedStateVars[contractName])
+      // For ContractDefinition
+
+      // For UsingForDeclaration
+      for (let usingForProfile of profile.usingForProfiles) {
+        libraryName = usingForProfile.libraryName
+      }
+      // For UsingForDeclaration
+
+      // For FunctionDefinition
+      for (let functionProfile of profile.functionProfiles) {
+        if (functionProfile.name === 'constructor') {
+          functionName = '<Constructor>'
+        } else if (functionProfile.name === 'fallback') {
+          functionName = '<Fallback>'
+        } else {
+          functionName = functionProfile.name
+        }
+
+        let spec = ''
+        if (functionProfile.visibility === 'public' || functionProfile.visibility === 'default') {
+          spec += '[Pub] ❗️'
+        } else if (functionProfile.visibility === 'external') {
+          spec += '[Ext] ❗️'
+        } else if (functionProfile.visibility === 'private') {
+          spec += '[Priv] 🔐'
+        } else if (functionProfile.visibility === 'internal') {
+          spec += '[Int] 🔒'
+        }
+
+        let payable = ''
+        if (functionProfile.mutability === 'payable') {
+          payable = '💵'
+        }
+
+        let mutating = ''
+        if (!functionProfile.mutability) {
+          mutating = '🛑'
+        }
+
+        functionDecorators[functionName] = ` | ${spec}  ${mutating} ${payable}`
+
+        functionCallsTree[contractName][functionName] = {}
+       // console.log("fun:contractName="+contractName)
+        //console.log("fun:functionName="+functionName)
+        modifiers[contractName][functionName] = new Array()
+        //console.log(modifiers[contractName][functionName])
+      }
+      // For FunctionDefinition
+
+      // For ModifierDefinition
+      for (let modifierDefProfile of profile.modifierDefProfiles) {
+        functionName = modifierDefProfile.name
+
+        modifiers[contractName][functionName] = new Array()
+
+        functionCallsTree[contractName][functionName] = {}
+      }
+      // For ModifierDefinition
+
+      // For ModifierInvocation
+      for (let modifierInvocProfile of profile.modifierInvocProfiles) {
+      //  console.log("modename1="+functionName)
+      //console.log("modifierInvocProfile1.name="+modifierInvocProfile.name)
+
+        //console.log(modifiers[contractName][functionName])
+
+        modifiers[contractName][functionName].push(modifierInvocProfile.name)
+      }
+      // For ModifierInvocation
+
+      // For ParameterList
+       for (let paramListProfile of profile.paramListProfiles) {
+        for (let parameter of paramListProfile.parameters) {
+          if (parserHelpers.isUserDefinedDeclaration(parameter)) {
+            userDefinedLocalVars[parameter.name] = parameter.typeName.name
+          } else if (parserHelpers.isAddressDeclaration(parameter)) {
+            // starting name with  "#" because it's an illegal character for naming vars in Solidity
+            userDefinedLocalVars[parameter.name] = `#address [${parameter.name}]`
+          }
+        }
+      }
+      // For ParameterList
+
+      // For VariableDeclaration
+      for (let varProfile of profile.varProfiles) {
+        let node = varProfile.node
+        let name = varProfile.name
+        if (functionName && parserHelpers.isUserDefinedDeclaration(node)) {
+          userDefinedLocalVars[name] = varProfile.nodeTypePath
+        } else if (functionName && parserHelpers.isAddressDeclaration(node)) {
+          // starting name with  "#" because it's an illegal character for naming vars in Solidity
+          userDefinedLocalVars[name] = `#address [${name}]`
+        }
+      }
+     // For VariableDeclaration
+
+     // For FunctionCall
+      for (let funCallProfile of profile.funCallProfiles) {
+         if (!functionName) {
+           // this is a function call outside of functions and modifiers, ignore if exists
+           console.log("outside function call")
+           return
+         }
+
+         const expr = funCallProfile.expr
+         let node = funCallProfile.node
+         console.log("inside FunctionCall")
+         console.log(expr)
+
+         let name
+         let localContractName
+         let visibility
+
+         // The following block is a nested switch statement for creation of the call tree
+         // START BLOCK
+         if (parserHelpers.isRegularFunctionCall(node)) {
+           name = expr.name
+           localContractName = contractName
+           console.log("isRegularFunctionCall,name="+name)
+           visibility = 'internal'
+         } else if (parserHelpers.isMemberAccess(node)) {
+           let object
+
+           visibility = 'external'
+
+           name = expr.memberName
+
+           if (expr.expression.hasOwnProperty('name')) {
+             object = expr.expression.name
+
+           // checking if it is a member of `address` and pass along it's contents
+           } else if (parserHelpers.isMemberAccessOfAddress(node)) {
+             if(expr.expression.arguments[0].hasOwnProperty('name')) {
+               object = expr.expression.arguments[0].name
+             } else {
+               object = JSON.stringify(expr.expression.arguments)
+             }
+
+           // checking if it is a typecasting to a user-defined contract type
+           } else if (parserHelpers.isAContractTypecast(node)) {
+
+             if(expr.expression.expression.hasOwnProperty('name')) {
+               object = expr.expression.expression.name
+             } else {
+               console.log("Anurag0")
+               return
+             }
+           } else {
+             console.log("Anurag0-1")
+             return
+           }
+
+           console.log("Anurag1")
+           // Special keywords cases: this, super
+           if (object === 'this') {
+             localContractName = contractName
+           } else if (object === 'super') {
+             localContractName = dependencies[contractName][1]
+           // the next two cases are checking if any user defined contract variable members were accessed
+           } else if (tempUserDefinedStateVars[object] !== undefined) {
+             localContractName = tempUserDefinedStateVars[object]
+           } else if (userDefinedLocalVars[object] !== undefined) {
+             localContractName = userDefinedLocalVars[object]
+           } else if (libraryName !== undefined) {
+             localContractName = libraryName
+             console.log("Anurag2,localContractName="+libraryName)
+           } else {
+             localContractName = object
+           }
+         } else {
+           console.log("Anurag2")
+           return
+         }
+         // END BLOCK
+           console.log("Anurag3")
+         if(!functionCallsTree[contractName][functionName].hasOwnProperty(name)) {
+           functionCallsTree[contractName][functionName][name] = {
+             contract: localContractName,
+             numberOfCalls: 1,
+             visibility: visibility
+           }
+         } else {
+           functionCallsTree[contractName][functionName][name].numberOfCalls++
+         }
+      }
+      // For FunctionCall
+    }
+  }
+  // END of file traversing
+
+/*
+  for (let ast of fileASTs) {
+
+    let contractName = null
+    let libraryName = null
     let functionName = null
     let cluster = null
 
@@ -91,6 +304,11 @@ export function ftrace(functionId, accepted_visibility, files) {
     parser.visit(ast, {
       ContractDefinition(node) {
         contractName = node.name
+
+        if(node.kind == "library")
+          dependencies[contractName] = node.name
+
+      //  console.log("dependencies[contractName]="+dependencies[contractName])
 
         functionCallsTree[contractName] = {}
         modifiers[contractName] = {}
@@ -103,8 +321,13 @@ export function ftrace(functionId, accepted_visibility, files) {
       },
 
       'ContractDefinition:exit': function(node) {
-        contractName = null 
+        contractName = null
         tempUserDefinedStateVars = {}
+      },
+
+      UsingForDeclaration(node) {
+        libraryName = node.libraryName
+        console.log("lib="+libraryName)
       },
 
       FunctionDefinition(node) {
@@ -190,6 +413,8 @@ export function ftrace(functionId, accepted_visibility, files) {
         }
 
         const expr = node.expression
+          console.log("inside Func")
+          //console.log(expr)
 
         let name
         let localContractName
@@ -201,8 +426,10 @@ export function ftrace(functionId, accepted_visibility, files) {
           name = expr.name
           localContractName = contractName
           visibility = 'internal'
+          console.log("anurag1")
         } else if (parserHelpers.isMemberAccess(node)) {
           let object
+          console.log("anurag2")
 
           visibility = 'external'
 
@@ -241,10 +468,13 @@ export function ftrace(functionId, accepted_visibility, files) {
             localContractName = tempUserDefinedStateVars[object]
           } else if (userDefinedLocalVars[object] !== undefined) {
             localContractName = userDefinedLocalVars[object]
+          } else if (libraryName !== undefined) {
+            localContractName = libraryName
           } else {
             localContractName = object
           }
         } else {
+          console.log("anurag3")
           return
         }
         // END BLOCK
@@ -261,8 +491,8 @@ export function ftrace(functionId, accepted_visibility, files) {
       }
     })
   }
-  // END of file traversing
-
+  // working END of file traversing
+*/
   let touched = {}
   let callTree = {}
 
@@ -294,15 +524,18 @@ export function ftrace(functionId, accepted_visibility, files) {
   // Function to recursively generate the tree to show in the console
   function constructCallTree(reduceJobContractName, reduceJobFunctionName, parentObject) {
     let tempIterable
+    console.log("inside constructCallTree")
 
     if (functionCallsTree[reduceJobContractName][reduceJobFunctionName] === undefined) {
       return
     }
-
+  //  console.log("inside constructCallTree1")
     tempIterable = functionCallsTree[reduceJobContractName][reduceJobFunctionName]
 
+      console.log(modifiers[reduceJobContractName][reduceJobFunctionName])
     for (const modifier of modifiers[reduceJobContractName][reduceJobFunctionName]) {
       Object.assign(tempIterable, modifierCalls(modifier, reduceJobContractName))
+   //   console.log("modifier="+modifier)
     }
 
     Object.entries(tempIterable).forEach(([functionCallName, functionCallObject]) => {
@@ -339,7 +572,8 @@ export function ftrace(functionId, accepted_visibility, files) {
   }
 
   // Call with seed
+  console.log("before constructCallTree")
   constructCallTree(contractToTraverse, functionToTraverse, callTree[seedKeyString])
-
+console.log("after constructCallTree")
   console.log(treeify.asTree(callTree, true))
 }
--- src/utils/contractProfiler.js
+++ src/utils/contractProfiler.js
@@ -2,49 +2,75 @@
 
 const fs = require('fs')
 const parser = require('solidity-parser-antlr')
+const path = require('path');
 
 /**
  * Given a solidity file, returns an array of objects, each one representing a contract
  *
  * @param      {string}  file    The file
  * @returns    {array}    contractProfiles A list of objects with details about a given contract
- */ 
+ */
 function contractProfilesFromFile(file) {
   let contractProfiles = new Array()
   let contractProfile = new Object()
   let functionProfile = new Object()
   let functionProfiles = new Array()
-
+  let usingForProfile = new Object()
+  let usingForProfiles = new Array()
+  let importProfile = new Object()
+  let importProfiles = new Array()
+  let stateVariableProfile = new Object()
+  let stateVariableProfiles = new Array()
+  let modifierDefProfile = new Object()
+  let modifierDefProfiles = new Array()
+  let modifierInvocProfile = new Object()
+  let modifierInvocProfiles = new Array()
+  let paramListProfile = new Object()
+  let paramListProfiles = new Array()
+  let varProfile = new Object()
+  let varProfiles = new Array()
+  let funCallProfile = new Object()
+  let funCallProfiles = new Array()
 
   const content = fs.readFileSync(file).toString('utf-8')
   const ast = parser.parse(content)
 
   // Object to track which contract definition we are in.
-  
+
 
   parser.visit(ast, {
     ContractDefinition(node) {
-      // functionProfile = new Object() // Info about the current function definition 
+      // functionProfile = new Object() // Info about the current function definition
       // functionProfiles = new Array() // list of functions defined in this contract
 
       let name = node.name
-      
+
       let bases = node.baseContracts.map(spec => {
         return spec.baseName.namePath
       })
 
       let kind = node.kind
-      
-      Object.assign(contractProfile, {name, bases, kind})
+
+      Object.assign(contractProfile, {name, bases, kind, ast})
     },
 
     'ContractDefinition:exit': function(node) {
       // Done visiting this contract
-      Object.assign(contractProfile, { functionProfiles }) // record function details
+      Object.assign(contractProfile, {
+        functionProfiles,
+        usingForProfiles,
+        importProfiles,
+        stateVariableProfiles,
+        modifierDefProfiles,
+        modifierInvocProfiles,
+        paramListProfiles,
+        varProfiles,
+        funCallProfiles
+      }) // record function details
       contractProfiles.push(contractProfile) // add to list of contractProfiles
       contractProfile = new Object() // reset the contractProfile object
     },
-    
+
     FunctionDefinition(node) {
       let name = ''
 
@@ -65,11 +91,100 @@ function contractProfilesFromFile(file) {
 
       Object.assign(functionProfile, {name, visibility, mutability})
 
-    }, 
+    },
     'FunctionDefinition:exit': function(node) {
       functionProfiles.push(functionProfile)
       functionProfile = new Object() //
-    }
+    },
+
+    UsingForDeclaration(node) {
+      let libraryName = node.libraryName
+      Object.assign(usingForProfile, {libraryName})
+    },
+    'UsingForDeclaration:exit': function(node) {
+      usingForProfiles.push(usingForProfile)
+      usingForProfile = new Object() //
+    },
+
+    ImportDirective: function(node) {
+      let filename = path.parse(node.path).base;
+      const [libName, libExt] = filename.split('.', 2)
+      const [conPath, conName] = file.split('/', 2)
+      let lib = conPath+"/"+node.path
+      let content1;
+      try {
+        content1 = fs.readFileSync(lib).toString('utf-8')
+      } catch (e) {
+        if (e.code === 'EISDIR') {
+          console.error(`Skipping directory ${lib}`)
+        } else throw e;
+      }
+
+      const astImport = parser.parse(content1)
+
+      Object.assign(importProfile, {astImport})
+    },
+    'ImportDirective:exit': function(node) {
+      importProfiles.push(importProfile)
+      importProfile = new Object() //
+    },
+
+    StateVariableDeclaration(node) {
+      let variables = node.variables
+      Object.assign(stateVariableProfile, {variables})
+    },
+    'StateVariableDeclaration:exit': function(node) {
+      stateVariableProfiles.push(stateVariableProfile)
+      stateVariableProfile = new Object() //
+    },
+
+    ModifierDefinition(node) {
+      let name = node.name
+      Object.assign(modifierDefProfile, {name})
+    },
+    'ModifierDefinition:exit': function(node) {
+      modifierDefProfiles.push(modifierDefProfile)
+      modifierDefProfile = new Object() //
+    },
+
+    ModifierInvocation(node) {
+      let name = node.name
+      Object.assign(modifierInvocProfile, {name})
+    },
+    'ModifierInvocation:exit': function(node) {
+      modifierInvocProfiles.push(modifierInvocProfile)
+      modifierInvocProfile = new Object() //
+    },
+
+    ParameterList(node) {
+      let parameters = node.parameters
+      Object.assign(paramListProfile, {parameters})
+    },
+    'ParameterList:exit': function(node) {
+      paramListProfiles.push(paramListProfile)
+      paramListProfile = new Object() //
+    },
+
+    VariableDeclaration(node) {
+      let name = node.name
+      let nodeTypePath = node.typeName.namePath
+      Object.assign(varProfile, {name, nodeTypePath, node})
+    },
+    'VariableDeclaration:exit': function(node) {
+      varProfiles.push(varProfile)
+      varProfile = new Object() //
+    },
+
+    FunctionCall(node) {
+      let expr = node.expression
+
+      Object.assign(funCallProfile, {expr, node})
+    },
+    'FunctionCall:exit': function(node) {
+      funCallProfiles.push(funCallProfile)
+      funCallProfile = new Object() //
+    },
+
   })
 
   return contractProfiles
