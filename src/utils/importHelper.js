3fcafd9a1d04287ae985c1f97743bdd978cad04a
maurelian <maurelian@protonmail.ch>
2018-09-17 16:06:16 -0400

    Add importHelper

---
+++ src/utils/importHelper.js
@@ -0,0 +1,81 @@
+"use strict";
+
+const fs = require('fs')
+const path = require('path');
+const parser = require('solidity-parser-antlr')
+
+
+/// Given a solidity file, returns an array of paths to be imported
+/// 
+/// @param      {string}  filePath  The file
+/// @param      {Array}   paths     The paths
+/// @return     {array}  importPaths A list of importPaths
+function importPathsFromFile(filePath, paths = []) {
+  paths.push(filePath)
+
+  const content = fs.readFileSync(filePath).toString('utf-8')
+  const ast = parser.parse(content)
+  
+  parser.visit(ast, {
+    ImportDirective(node) {
+      paths.push(resolveImportPath(filePath, node.path))
+      importPathsFromFile(`${workingDir}/${node.path}`, paths)
+    }
+  })
+  return paths
+}
+
+
+/// Takes a filepath, and an import path found within it, and finds the corresponding source code
+/// file. Throws and error if the resolved path is not a file.
+///
+/// @param      {<type>}  baseFilePath      The base file path
+/// @param      {<type>}  relativeFilePath  The relative file path
+///
+function resolveImportPath(baseFilePath, relativeFilePath){
+  let resolvedPath;
+  let baseDirPath = path.parse(baseFilePath).dir
+  // if it's a relative path:
+  if (
+    relativeFilePath.slice(0,2) === './' 
+    || relativeFilePath.slice(0,3) === '../'
+    || relativeFilePath.slice(0,1) === '/'
+  ) 
+  {
+    resolvedPath = path.resolve(baseDirPath, relativeFilePath)
+  // else it's most likely a special case using a remapping to node_modules dir
+
+  } else {
+    let nodeModulesPath = findNodeModules(baseFilePath)
+    resolvedPath = path.resolve(nodeModulesPath, relativeFilePath)
+  }
+
+  // verify that the resolved path is actually a file
+  if (
+    !fs.existsSync(resolvedPath) 
+    || !fs.statSync(resolvedPath).isFile()
+  ) {
+    throw new Error(`Import path (${resolvedPath}) not resolved to a file`)
+  }
+  return resolvedPath
+}
+
+/// starts with a current path, and moves up dirs until it finds node_modules dir
+///
+/// @param      {<type>}  currentPath  The current path
+/// @return     {<type>}  { description_of_the_return_value }
+function findNodeModules(currentPath){
+  let testPath = `${currentPath}/node_modules`
+  if (fs.existsSync(testPath) 
+    && fs.statSync(testPath).isDirectory()
+  ) {
+    return testPath
+  } else {
+    let parentPath = path.resolve(currentPath, '..')
+    if(parentPath === '/') throw new Error('node_modules not found')
+    return findNodeModules(`${parentPath}`)
+  }
+}
+
+let paths = importPathsFromFile(process.argv[2]);
+console.log(paths)
