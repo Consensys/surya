0aa3c34288901241d6458e58cd304ff86df524fc
maurelian <leighvert@protonmail.ch>
2019-04-16 10:30:09 -0400

    Bring importHelper from feat/derivedProfile
    WIP
    
    More work on content replacement functionality
    
    partly working to flatten
    
    Save before strategy change
    
    Seems to work

--- src/flatten.js
+++ src/flatten.js
@@ -1,39 +1,70 @@
 "use strict";
 
+import { resolve } from 'url';
+
 const fs = require('fs')
 const parser = require('solidity-parser-antlr')
-const { dependencies } = require('./dependencies')
+const { resolveImportPath } = require('./utils/importHelper')
 
-
-export function flatten(files, targetContract) {
-  if (files.length === 0) {
-    console.log('No files were specified for analysis in the arguments. Bailing...')
+export function flatten(files) {
+  if (!files) {
+    console.log('No file was specified for analysis in the arguments. Bailing...')
     return
   }
 
-  if (!targetContract) {
-    console.log('No target contract specified in the arguments. Bailing.. ')
-    return
+  // create a set of paths already inserted to pass while flattening each file, to avoid duplication
+  let visitedPaths = new Set()
+  let flat = ''
+  for (let file of files){
+    flat += replaceImportsWithSource(file, visitedPaths)
+    flat += '\n'
+  }
+  console.log(flat)
+}
+
+/**  
+ * Given a solidity file, returns the content with imports replaced by source code
+ * 
+ * @param      {string}  file  The file
+ * @param      {Array}   visitedPaths     Paths already resolved
+ * @return     {string}  resolvedContent A string with imports replaced by source code
+ */
+function replaceImportsWithSource(file, visitedPaths = new Set()) {
+  let content
+  try {
+    content = fs.readFileSync(file).toString('utf-8')
+  } catch (e) {
+    if (e.code === 'EISDIR') {
+      console.error(`Skipping directory ${file}`)
+    } else throw e;
   }
 
-  let derivedLinearization = dependencies(files, targetContract)
+  const ast = parser.parse(content, {
+    loc: true
+  })
 
-  for (let file of files) {
-
-    let content
-    try {
-      content = fs.readFileSync(file).toString('utf-8')
-    } catch (e) {
-      if (e.code === 'EISDIR') {
-        console.error(`Skipping directory ${file}`)
-        continue
-      } else throw e;
+  let importsAndLocations = [];
+  
+  parser.visit(ast, {
+    ImportDirective(node) {
+      let importPath = resolveImportPath(file, node.path)
+      importsAndLocations.push({ importPath, location: node.loc })
     }
-
-    const ast = parser.parse(content)
-
-
-  dependencies = linearize(dependencies, {reverse: true})
-  console.log()
-  return dependencies[targetContract]
+  })
+  
+  let contentLines = content.split('\n')
+  for (let el of importsAndLocations){
+    // array is 0 indexed, file lines are 1 indexed so we need to replace `start.line - 1`
+    if (visitedPaths.has(el.importPath)){
+      // we've already visited this path, just delete the import statement
+      contentLines[el.location.start.line - 1] = ''      
+    } else {
+      // first time handling this import path, 
+      contentLines[el.location.start.line - 1] = 
+        `// flattened from: ${el.importPath}  \n ${replaceImportsWithSource(el.importPath, visitedPaths)}`
+      visitedPaths.add(el.importPath)
+    }
+  }
+  
+  return contentLines.join('\n')
 }
