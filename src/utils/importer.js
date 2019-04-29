8950be52fb67dc44d5cf45aaf91b3f76d85ca2cf
Gonçalo Sá <goncalo05@gmail.com>
2019-04-29 10:54:33 +0100

    Add node_modules search + path traversal protection

--- src/utils/importer.js
+++ src/utils/importer.js
@@ -15,60 +15,72 @@ const parser = require('solidity-parser-antlr')
  * @param      {Set}     importedFiles  files already parsed 
  * @return     {Array}   importPaths    A list of importPaths 
  */
-function importProfiler(files, projectDir = process.cwd(), importedFiles = new Set()) {
-  for (let file of files){
-    // Checks for a valid solidity file
-    file = path.resolve(projectDir, file)
-    if (file.indexOf(projectDir) != 0) throw new Error(`Imports must be found in sub dirs of the projectDir: ${projectDir}`)
-    let content
-    try {
-      content = fs.readFileSync(file).toString('utf-8')
-    } catch (e) {
-      if (e.code === 'EISDIR') {
-        console.error(`Skipping directory ${file}`)
-        return importedFiles // empty Set
-      } else throw e;
-    }
-    // Having verified that it indeed is a solidity file, add it to set of importedFiles
-    importedFiles.add(file)
-    const ast = parser.parse(content, {tolerant: true})
-    
-    // create an array to hold the imported files
-    const newFiles = new Array()
-    parser.visit(ast, {
-      ImportDirective(node) {
-        let newFile = resolveImportPath(file, node.path)
-        newFiles.push(newFile)
+const importer = {
+  importProfiler: (files, projectDir = process.cwd(), importedFiles = new Set()) => {
+    for (let file of files){
+      // Checks for a valid solidity file
+      file = path.resolve(projectDir, file)
+      if (file.indexOf(projectDir) != 0) throw new Error(`Imports must be found in sub dirs of the projectDir: ${projectDir}`)
+      let content
+      try {
+        content = fs.readFileSync(file).toString('utf-8')
+      } catch (e) {
+        if (e.code === 'EISDIR') {
+          console.error(`Skipping directory ${file}`)
+          return importedFiles // empty Set
+        } else throw e;
       }
-    })
-    // Run through the array of files found in this file
-    importProfiler(newFiles, projectDir, importedFiles)
+      // Having verified that it indeed is a solidity file, add it to set of importedFiles
+      importedFiles.add(file)
+      const ast = parser.parse(content, {tolerant: true})
+      
+      // create an array to hold the imported files
+      const newFiles = new Array()
+      parser.visit(ast, {
+        ImportDirective(node) {
+          let newFile = resolveImportPath(file, node.path, projectDir)
+          newFiles.push(newFile)
+        }
+      })
+      // Run through the array of files found in this file
+      module.exports.importProfiler(newFiles, projectDir, importedFiles)
+    }
+    // Convert the set to an array for easy consumption
+    const importedFilesArray = Array.from(importedFiles)
+    return importedFilesArray
   }
-  // Convert the set to an array for easy consumption
-  const importedFilesArray = Array.from(importedFiles)
-  return importedFilesArray
 }
 
-module.exports = importProfiler
+module.exports = importer
 
 /// Takes a filepath, and an import path found within it, and finds the corresponding source code
 /// file. Throws an error if the resolved path is not a file.
 ///
-/// @param      {<type>}  baseFilePath      The base file path
-/// @param      {<type>}  relativeFilePath  The relative file path
+/// @param      {string}  baseFilePath      The base file path
+/// @param      {string}  importedFilePath  The imported file path
+/// @param      {string}  projectDir        The top-most directory we will search in
 ///
-function resolveImportPath(baseFilePath, relativeFilePath){
-  let resolvedPath;
-  let baseDirPath = path.parse(baseFilePath).dir
+function resolveImportPath(baseFilePath, importedFilePath, projectDir){
+  let resolvedPath
+  let baseDirPath = path.dirname(baseFilePath)
   // if it's a relative path:
   if (
-    relativeFilePath.slice(0,2) === './' 
-    || relativeFilePath.slice(0,3) === '../'
+    importedFilePath.slice(0,2) === './' 
+    || importedFilePath.slice(0,3) === '../'
   ) {
-    resolvedPath = path.resolve(baseDirPath, relativeFilePath)
-  // else it's most likely a special case using a remapping to node_modules dir
+    resolvedPath = path.resolve(baseDirPath, importedFilePath)
+  // else it's most likely a special case using a remapping to node_modules dir in Truffle
   } else {
-    // TODO: handle remappings
+    const topmostDirArray = projectDir.split(path.sep)
+    let currentDirArray = baseDirPath.split(path.sep)
+    let currentDirName = currentDirArray.pop()
+    while (currentDirName != 'contracts') {
+      if (topmostDirArray.length == currentDirArray.length) {
+        throw new Error(`Import statement seems to be a Truffle "'node_modules' remapping" but no 'contracts' truffle dir could be found in the project's child dirs. Have you ran 'npm install', already? (path: ${baseDirPath})`)
+      }
+      currentDirName = currentDirArray.pop()
+    }
+    resolvedPath = path.join('/', ...currentDirArray, 'node_modules', importedFilePath)
   }
 
   // verify that the resolved path is actually a file
