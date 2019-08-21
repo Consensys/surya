4e228b5a1648d94b41a89bef7f2edba30c742dc8
Gonçalo Sá <goncalo05@gmail.com>
2019-08-21 11:41:40 -0700

    Signal code location for wanted changes

--- src/ftrace.js
+++ src/ftrace.js
@@ -233,6 +233,7 @@ export function ftrace(functionId, accepted_visibility, files, options = {}, noC
       FunctionCall(node) {
         if (!functionName) {
           // this is a function call outside of functions and modifiers, ignore if exists
+          // TODO: THIS IS ACTUALLY THE CONSTRUCTOR :facepalm:
           return
         }
 
