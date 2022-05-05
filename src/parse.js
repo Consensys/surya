c8c7f9c31ca7f1d8059aa4a6b27deb1ec1156335
Matthew Tam <matthewethan.tam@gmail.com>
2022-05-05 12:09:34 +0200

    added some changes

--- src/parse.js
+++ src/parse.js
@@ -1,42 +1,44 @@
 "use strict";
 
-const fs = require('fs');
-const parser = require('@solidity-parser/parser');
-const treeify = require('treeify');
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.parse = parse;
+var fs = require('fs');
+var parser = require('@solidity-parser/parser');
+var treeify = require('treeify');
 
-export function parse(file) {
-    var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
-    const content = fs.readFileSync(file).toString('utf-8');
-    const ast = (() => {
+function parse(file, options = {}) {
+    // var options = arguments.length > 4 && arguments[1] !== undefined ? arguments[1] : {};
+    var content = fs.readFileSync(file).toString('utf-8');
+    var ast = function () {
         try {
-            if (options.tolerance && !options.location && !options.range) {
-                return parser.parse(content, {tolerance: true});
-            } else if (options.tolerance && options.location && !options.range) {
-                return parser.parse(content, {tolerance: true, location: true});
-            } else if (options.tolerance && options.location && options.range) {
-                return parser.parse(content, {tolerance: true, location: true, range: true});
-            } else if (!options.tolerance && options.location && options.range) {
-                return parse.parse(content, {location: true, range: true});
-            } else if (!options.tolerance && options.location && !options.range) {
-                return parse.parse(content, {location: true});
-            } else if (!options.tolerance && !options.location && options.range) {
-                return parse.parse(content, {range: true});
-            } else {
-                return parser.parse(content);
-            }
+            // if (options.tolerance && !options.location && !options.range) {
+            //     return parser.parse(content, { tolerance: true });
+            // } else if (options.tolerance && options.location && !options.range) {
+            //     return parser.parse(content, { tolerance: true, loc: true });
+            // } else if (options.tolerance && options.location && options.range) {
+            //     return parser.parse(content, { tolerance: true, loc: true, range: true });
+            // } else if (!options.tolerance && options.location && options.range) {
+            //     return parse.parse(content, { loc: true, range: true });
+            // } else if (!options.tolerance && options.location && !options.range) {
+            //     return parse.parse(content, { loc: true });
+            // } else if (!options.tolerance && !options.location && options.range) {
+            //     return parse.parse(content, { range: true });
+            // } else {
+                return parser.parse(content, { tolerance: options.tolerance, loc: options.location, range: options.range });
+            // }
         } catch (err) {
-            console.log(`Error found while parsing the following file: ${file}`);
+            console.log('Error found while parsing the following file: ' + file);
             throw err;
         }
-    })();
+    }();
 
     if (options.jsonOutput) {
 
         return JSON.stringify(ast);
-
     } else {
 
         return treeify.asTree(ast, true);
     }
-    
 }
