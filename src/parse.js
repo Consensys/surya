a8e24003eea4155572d5a76b17e1130211f5a9f5
Matthew Tam <matthewethan.tam@gmail.com>
2022-05-05 14:02:07 +0200

    fixing changes

--- src/parse.js
+++ src/parse.js
@@ -9,25 +9,10 @@ var parser = require('@solidity-parser/parser');
 var treeify = require('treeify');
 
 function parse(file, options = {}) {
-    // var options = arguments.length > 4 && arguments[1] !== undefined ? arguments[1] : {};
     var content = fs.readFileSync(file).toString('utf-8');
     var ast = function () {
         try {
-            // if (options.tolerance && !options.location && !options.range) {
-            //     return parser.parse(content, { tolerance: true });
-            // } else if (options.tolerance && options.location && !options.range) {
-            //     return parser.parse(content, { tolerance: true, loc: true });
-            // } else if (options.tolerance && options.location && options.range) {
-            //     return parser.parse(content, { tolerance: true, loc: true, range: true });
-            // } else if (!options.tolerance && options.location && options.range) {
-            //     return parse.parse(content, { loc: true, range: true });
-            // } else if (!options.tolerance && options.location && !options.range) {
-            //     return parse.parse(content, { loc: true });
-            // } else if (!options.tolerance && !options.location && options.range) {
-            //     return parse.parse(content, { range: true });
-            // } else {
                 return parser.parse(content, { tolerance: options.tolerance, loc: options.location, range: options.range });
-            // }
         } catch (err) {
             console.log('Error found while parsing the following file: ' + file);
             throw err;
